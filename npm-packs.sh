935860f525647c5baeb34ee870dafbffeb5b5325
Stephen <flowstepg@gmail.com>
2018-10-02 12:48:44 +1000

    Add gulp package into npm-packs

--- npm-packs.sh
+++ npm-packs.sh
@@ -1,2 +1,2 @@
-npm i del browser-sync gulp-load-plugins gulp-sourcemaps gulp-autoprefixer gulp-cheerio gulp-concat gulp-csscomb gulp-csso gulp-if gulp-notify gulp-pug gulp-replace gulp-sass gulp-svg-sprite gulp-svgmin gulp-tinypng gulp-uglifyjs --save-dev
+npm i del gulp@next browser-sync gulp-load-plugins gulp-sourcemaps gulp-autoprefixer gulp-cheerio gulp-concat gulp-csscomb gulp-csso gulp-if gulp-notify gulp-pug gulp-replace gulp-sass gulp-svg-sprite gulp-svgmin gulp-tinypng gulp-uglifyjs --save-dev
 npm i svg4everybody --save-prod
